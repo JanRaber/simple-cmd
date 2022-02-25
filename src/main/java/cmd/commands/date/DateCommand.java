489c5b3a19c98aa5cad670d8bdb1df84d115c0e1
mnguetsa <michael-klaus.nguetsa-dongmo@capgemini.com>
2022-02-25 15:11:45 +0100

    Adds date Command class

---
+++ src/main/java/cmd/commands/date/DateCommand.java
@@ -0,0 +1,32 @@
+package cmd.commands.date;
+
+import cmd.SimpleCmd;
+import picocli.CommandLine.Command;
+
+import java.io.File;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import static picocli.CommandLine.Option;
+
+@Command(
+        name = "date",
+        description = "command can be used to print the date",
+        mixinStandardHelpOptions = true)
+public class DateCommand implements Runnable {
+
+    public DateCommand() {
+    }
+
+    @Override
+    public void run() {
+        Date actualDate=new Date();
+        String pattern = "yyy-mm-dd";
+        System.out.println();
+    }
+
+    
+}
